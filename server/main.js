16896a4de1731e91a2a2a5a1acc45e667637bc25
Ivan Duarte <duartiv@gmail.com>
2017-12-12 06:46:44 -0500

    Adding Lang for Accessibility Improvements (WCAG 2.0, Guideline 3.1.1)
    Adding HTML Lang for Accessibility Improvements (WCAG 2.0, Guideline 3.1.1)
    https://github.com/GoogleChrome/accessibility-developer-tools/wiki/Audit-Rules#ax_html_014

--- server/main.js
+++ server/main.js
@@ -1,12 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import {Layer, Network} from 'synaptic';
 import { ServiceConfiguration } from 'meteor/service-configuration';
+import { Inject } from "meteor/meteorhacks:inject-initial";
+
 
 import '../imports/api/compositions.js';
 import '../imports/api/melodies.js';
 import '../imports/api/users.js';
 
+
 Meteor.startup(() => {
+  
+    // add HTML Lang 
+  Inject.rawModHtml("addLanguage", function(html) {
+    return html.replace(/<html>/, '<!-- HTML 5 -->\n<html lang="en">');
+  });
+  
   ServiceConfiguration.configurations.upsert(
   	{ service: 'facebook' },
   	{
